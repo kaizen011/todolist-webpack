0b2936c521f2251b47f66a93551bc009f91e80cd
kaizen011 <125158784+kaizen011@users.noreply.github.com>
2023-08-24 18:22:26 -0500

    Added toDoList functionality, unfinished but somewhat finished project, as for design.. certainly unfinished.

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -55,9 +55,58 @@ function createAndAppendElements(){
     todoContainer.appendChild(todoList);
 
     contentDiv.appendChild(todoContainer);
+
     
     // To Do List Logic
-    
+    function addTask(){
+        const taskText = taskInput.value.trim();
+        const priorityValue = prioritySelect.value;
+        if (taskText !== ""){
+            const newTask = document.createElement("li");
+            newTask.textContent = `${taskText} (Priority:${priorityValue})`;
+
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = 'Delete task';
+            deleteButton.addEventListener("click", function(){
+                newTask.remove();
+            });
+
+            const editButton = document.createElement("button");
+            editButton.textContent = 'Edit task';
+            editButton.addEventListener('click', function(){
+                const editInput = document.createElement("input");
+                editInput.value = taskText;
+                
+
+                const saveButton = document.createElement("button");
+                saveButton.textContent = "Save";
+                saveButton.addEventListener("click", function(){
+                    const editedText = editInput.value;
+                    newTask.textContent = `${editedText} (Priority ${priorityValue})`;
+                    newTask.appendChild(deleteButton);
+                    newTask.appendChild(editButton);
+                });
+
+                newTask.textContent = "";
+                newTask.appendChild(editInput);
+                newTask.appendChild(saveButton);
+            });
+
+            newTask.appendChild(deleteButton);
+            newTask.appendChild(editButton);
+            todoList.appendChild(newTask);
+
+            taskInput.value = "";
+        }
+    }
+
+    addButton.addEventListener("click", addTask);
+
+    taskInput.addEventListener("keyup", function(event){
+        if(event.key === "Enter"){
+            addTask();
+        }
+    });
     
      return contentDiv;
 }
